d8fcecf95c77ef523f57fd22564dc6ffcda68619
PavanCarol <pavanm.carolina@gmail.com>
2026-01-08 16:39:33 -0300

    --criacao do projeto

---
+++ Festivite/src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+//Para rodar: 
+function App() {
+
+  return (
+    <>
+     <div className='title'>
+      Crie seu convite
+     </div>
+     <div>
+      <div className='header'>
+        Sobre o evento
+      </div>
+      <div>
+        <div className='title-body'>Título</div>
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default App
